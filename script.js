8fb6a23d190d39adc9ddff6c9063ee13a6a89683
Daniela Mendez <dannyemet@gmail.com>
2022-09-30 21:34:53 -0500

    feat: se arreglo error

--- script.js
+++ script.js
@@ -8,37 +8,38 @@ const btnCalcular = document.getElementById("calculate");
 const resultadoInput = document.getElementById("result");
 const dias = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
 
-const diaLaboral = (findeSemana) => {
-    switch (findeSemana) {
-        case 1:
+const diaLaboral = (findesemana) => {
+    switch (findesemana) {
+        
+        case `1`:
 
-        case 2:
+        case `2`:
 
-        case 3:
+        case `3`:
 
-        case 4:
+        case `4`:
 
-        case 5:
+        case `5`:
 
-            return "día laboral"
-
-            default: "fin de semana"
-            break;
+        return "día laboral"
+           
+        default: 
+        return"descanso"
+        break;
 
     }
 }
 
 const calculate = () => {
-    const dd = new Date(yearInput.value, monthInput.value -1 , dayInput.value);
-    const findeSemana = dd.getDay();
-    resultadoInput.value = `${dias[findeSemana][0] + dias[findeSemana].slice(1)}, ${diaLaboral(findeSemana)}`
+    const dd = new Date(yearInput.value, monthInput.value -1, dayInput.value);
+    const findesemana = dd.getDay()
+    resultadoInput.value = `${dias[findesemana][0] + dias[findesemana].slice(1)}, ${diaLaboral(findesemana)}`;
 
 }
 
 
-const fineInputs = event => {
-    event.preventDefault();
-    yearInput.value && monthInput.value && dayInput.value ? calculate() : alert('');
+const fineInputs = () => {  
+    yearInput.value && monthInput.value && dayInput.value ? calculate() : alert('Inputs');
 }
 
 btnCalcular.addEventListener("click", fineInputs); 
